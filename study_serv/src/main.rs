eb7f9f65a2c41d5eb1fb67fd6ecfbe3be6e3baa9
HJin.me <hjin.me@qq.com>
2023-11-19 16:53:23 +0800

    feat: 增加几个启动参数

--- study_serv/src/main.rs
+++ study_serv/src/main.rs
@@ -26,54 +26,63 @@ use serde::{Deserialize, Serialize};
 use std::time::Duration;
 use tracing::{info, trace};
 
+#[cfg(feature = "web")]
 fn main() {
-    #[cfg(feature = "web")]
-    {
-        tracing_wasm::set_as_global_default();
-        LaunchBuilder::new_with_props(app, ()).launch();
+    tracing_wasm::set_as_global_default();
+    LaunchBuilder::new_with_props(app, ()).launch();
+}
+
+#[cfg(any(not(feature = "web"), feature = "ssr"))]
+#[tokio::main]
+async fn main() {
+    use axum::routing::*;
+    use axum::Extension;
+    use clap::Parser;
+    use study::{bb8, StateSession, XxManager};
+
+    #[derive(Parser, Debug)]
+    #[command(author, version, about, long_about = None)]
+    struct Args {
+        /// Number of times to greet
+        #[arg(long, default_value = "10")]
+        max_size: u32,
+        #[arg(long, default_value = "1")]
+        min_idle: u32,
     }
 
-    #[cfg(feature = "ssr")]
-    {
-        use axum::routing::*;
-        use axum::Extension;
-        use study::{bb8, StateSession, XxManager};
-        tokio::runtime::Runtime::new()
-            .unwrap()
-            .block_on(async move {
-                let _g = crate::backend::otel::init_tracing_subscriber("study");
-                trace!("Starting up");
-                let manager = XxManager::new();
-                trace!("init browsers");
-                let pool = bb8::Pool::builder()
-                    .max_size(10)
-                    .min_idle(Some(1))
-                    .idle_timeout(Some(Duration::from_secs(170)))
-                    .build(manager)
-                    .await
-                    .unwrap();
+    let args = Args::parse();
 
-                trace!("init sessions");
-                let ss = StateSession::new(&pool);
+    let _g = backend::otel::init_tracing_subscriber("study");
+    trace!("Starting up, {:?}", args);
+    let manager = XxManager::new();
+    trace!("init browsers");
+    let pool = bb8::Pool::builder()
+        .max_size(args.max_size)
+        .min_idle(Some(args.min_idle))
+        .idle_timeout(Some(Duration::from_secs(170)))
+        .build(manager)
+        .await
+        .unwrap();
 
-                // build our application with some routes
-                let app = Router::new()
-                    // Server side render the application, serve static assets, and register server functions
-                    .serve_dioxus_application("/xx/api", ServeConfigBuilder::new(app, ()))
-                    .layer(Extension(pool))
-                    .layer(Extension(ss));
+    trace!("init sessions");
+    let ss = StateSession::new(&pool);
 
-                // run it
-                #[cfg(feature = "dev")]
-                let addr = std::net::SocketAddr::from(([127, 0, 0, 1], 3000));
-                #[cfg(not(feature = "dev"))]
-                let addr = std::net::SocketAddr::from(([0, 0, 0, 0], 3000));
+    // build our application with some routes
+    let app = Router::new()
+        // Server side render the application, serve static assets, and register server functions
+        .serve_dioxus_application("/xx/api", ServeConfigBuilder::new(app, ()))
+        .layer(Extension(pool))
+        .layer(Extension(ss));
 
-                info!("listening: http://{}", addr.to_string());
-                axum::Server::bind(&addr)
-                    .serve(app.into_make_service())
-                    .await
-                    .unwrap();
-            });
-    }
+    // run it
+    #[cfg(feature = "dev")]
+    let addr = std::net::SocketAddr::from(([127, 0, 0, 1], 3000));
+    #[cfg(not(feature = "dev"))]
+    let addr = std::net::SocketAddr::from(([0, 0, 0, 0], 3000));
+
+    info!("listening: http://{}", addr.to_string());
+    axum::Server::bind(&addr)
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
 }
